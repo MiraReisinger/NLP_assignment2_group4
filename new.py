d47c146a77e8dfe8c2a9949496661b9ec1d50f95
Mira Reisinger <67761190+MiraReisinger@users.noreply.github.com>
2022-02-25 16:53:08 +0100

    Add files via upload
    this is a updated version - supposed to run with an inputfile: token-goldpredicate-goldargument

---
+++ new.py
@@ -0,0 +1,65 @@
+import sys
+import pandas as pd
+import spacy
+
+def main(input_path=None):
+    """Read in the inputfile, extract predicates and possible arguments write them to a file."""
+    if not input_path:
+        input_path = sys.argv[1]  # path to file to analyze
+
+    data = []
+
+    # read in file, extract whole sentences
+    with open(input_path, 'r', encoding='utf-8') as infile:
+        for line in infile:
+            components = line.rstrip('\n').split()
+            tokens = components[0]
+            gold_p = components[1]
+            gold_a = components[2]
+
+    # create text out of sentences
+    nlp = spacy.load("en_core_web_sm")
+
+    # possible dependency realtions for arguments
+    arg_dep = ['nsubj', 'dobj', 'nmod', 'amod', 'ccomp', 'xcomp' 'conj']
+
+    # dependency pipeline
+    for doc in nlp.pipe(tokens):
+        for tok in doc:
+            token = tok.text
+            pos = tok.pos_
+            dependency = tok.dep_
+            head = tok.head
+            dependent = [t.text for t in tok.children]
+            constituent = [t.text for t in tok.subtree]
+
+            #finding possible predicates and arguments
+            if pos == 'VERB':
+                argument = 'V'
+
+            elif dependency in arg_dep:
+                argument = 'ARG'
+            else:
+                argument = '_'
+
+        # finding corresponding predicate
+            ancestors = []
+            if argument == 'ARG':
+                for ancestor in tok.ancestors:
+                    if ancestor.pos_ == 'VERB':
+                        ancestors.append(ancestor)
+                        break
+
+        # crate dictionary for export
+            base_dict = {'Token': token, 'POS': pos, 'dependency': dependency, 'head': head, 'dependent': dependent,
+                         'constituent': constituent, 'argument': argument, 'predicate': ancestors}
+            data.append(base_dict)
+
+    # safe results with header to tsv file
+    df = pd.DataFrame(data=data)
+    output_path = input_path.replace('.conll', '_pred_arg.txt')
+    df.to_csv(output_path, sep='\t', header=True, index=False)
+
+
+if __name__ == '__main__':
+    main()
