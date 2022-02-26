a02e45c91ad3b6a05fba30fbbd1a79276b0a6072
elena-theresa-weber <90104896+elena-theresa-weber@users.noreply.github.com>
2022-02-26 16:36:45 +0100

    Add files via upload

--- extract_pred_arg.py
+++ extract_pred_arg.py
@@ -3,6 +3,18 @@ import pandas as pd
 #inputpath of the file that needs to be extracted 
 inputfile = "/data/srl_univprop_en.dev.conll"
 
+to_be_removed = ['# newdoc id =', '# sent_id =','# text = ', '*', 'Foreign'] 
+#to_be_replaced = ['*']
+
+with open(inputfile, encoding='utf-8') as infile, open('data/updated_file.conll', 'w', encoding='utf-8')as newfile:
+    for line in infile:
+        if not any(to_be_removed in line for to_be_removed in to_be_removed):
+            #for line in infile:
+             #   line.replace('', '_')
+            newfile.write(line)
+
+inputfile_updated =  "/data/updated_file.conll"
+
 #creating headers
 header_gold = ["token_id", "token", "lemma", "pos", "pos again", "mood, number", "dependency relation number", "dependency relation", "dependency + number", "Space:After", "predicate", "argument"]    
  
